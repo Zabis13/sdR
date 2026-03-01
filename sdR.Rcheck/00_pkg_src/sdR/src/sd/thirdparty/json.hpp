c4b508fffb4c7352a39554e46510ef58af3500ba
Zabis13 <lbsbmsu@mail.ru>
2026-03-01 17:28:27 +0300

    Remove build artifacts and temp files from git tracking
    Update .gitignore and untrack: .Rproj.user/, .RData, .Rhistory,
    sdR.Rcheck/, TODO.md, src/**/*.o, and large vocab headers (~128MB).

--- sdR.Rcheck/00_pkg_src/sdR/src/sd/thirdparty/json.hpp
+++
@@ -1,3 +0,0 @@
-// Proxy header: delegates to json.hpp.inc to avoid R CMD check pragma warnings
-// json.hpp.inc is nlohmann/json (https://github.com/nlohmann/json), MIT license
-#include "json.hpp.inc"
--- sdR.Rcheck/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(sdR)
-
-test_check("sdR")
